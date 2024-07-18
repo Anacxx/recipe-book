8ae1fc0da7f00f7c817a4ab4a1411bd85e3b9681
Anacxx <anacarolina.clcx@gmail.com>
2024-07-18 15:16:07 -0300

    dia2

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,47 @@
+import { useState } from "react"
+import { faHome, faList, faCog } from '@fortawesome/free-solid-svg-icons';
+import Sidebar from "../sidebar/Sidebar"
+export default function Navbar(){
+    const [showSidebar, setShowSidebar] = useState(false)
+    const links = [
+        {
+            name: 'Home',
+            path: '/',
+            icon: faHome
+        },
+        {
+            name: 'Recipes',
+            path: '/recipes',
+            icon: faList
+        },
+        {
+            name: 'Settings',
+            path: '/settings',
+            icon: faCog
+        }
+    ]
+    function closeSidebar(){
+        setShowSidebar(false)
+    }
+    return(
+        <>
+            <div className="navbar container">
+                <a href="#!" className="logo">F<span>oo</span>diesHub</a>
+                <div className="nav-links">
+                    { links.map(link => (
+                        <a href="#!" key={link.name}>{link.name}</a>
+                    ))}
+                {/* <a href="#!">Home</a>
+                    <a href="#!">Recipes</a>
+                    <a href="#!">Settings</a> */}
+                </div>
+                <div onClick= {() => setShowSidebar(true) }className={showSidebar? "sidebar-btn active" : "sidebar-btn"} >
+                    <div className="bar"></div>                
+                    <div className="bar"></div>
+                    <div className="bar"></div>
+                </div>
+            </div>
+            { showSidebar && <Sidebar close = {closeSidebar} links={links}/> }
+        </>
+    )
+}
